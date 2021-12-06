2bce734238699945018bcf1a8b54d8bd5029f240
ashuya <despersonal45@gmail.com>
2021-12-07 01:16:14 +0500

    autosuggestion hotkey added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 set -g theme_displey_vi yes
 fish_vi_key_bindings
 alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias which='command -v'
+bind -M insert \cf forward-char
+bind -M insert \cg forward-word
 
